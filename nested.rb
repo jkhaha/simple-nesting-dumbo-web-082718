c7daa85b235fcd20ad82efc0e97664333884c6e8
Jackie Ha <jha2@wellesley.edu>
2018-08-21 14:18:33 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 def hopper
 	programmer_hash = 
  		{
@@ -127,8 +127,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-     programmer_hash[:dennis_ritchie][:languages]
-
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     return programmer_hash
 end
-binding.pry
-puts goodbye
